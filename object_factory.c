60f957e117a62cf52efc97727d81607b552efdb7
KaMaCH12 <kamachos12@gmail.com>
2020-01-14 19:16:31 +0100

    Zdjecia przeniesione do images

--- object_factory.c
+++ object_factory.c
@@ -117,7 +117,7 @@ int collision_checker(vector *v,player* SHIP)
     sfFloatRect obj_hitbox;
     sfFloatRect intersection;
     ship_hitbox=sfSprite_getGlobalBounds(SHIP->spr);
-    if(sfSprite_getPosition(SHIP->spr).y<0||sfSprite_getPosition(SHIP->spr).y>800-ship_hitbox.height)return 1;
+    if(sfSprite_getPosition(SHIP->spr).y+ship_hitbox.height<0||sfSprite_getPosition(SHIP->spr).y>800)return 1;
     for(int i=0;i<v->total;i++)
     {
 	OBJ=v->items[i];
